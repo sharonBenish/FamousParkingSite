d0c29e54e8b29fa79f8a733414f6d5e600a419fc
sharonBenish <sbenishwilliams@gmail.com>
2024-07-10 15:07:03 -0500

    update

---
+++ pages/use-cases.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<template>
+    <div class="hero relative wfull h60dvh 3xl:h70dvh bg-[url('/images/hero-overlay.jpg')] bg-cover bg-no-repeat -mt20 lg:-mt24">
+        <div class="hero-text text-white absolute top-80 lg:top-90 w-site right-1/2 left-1/2 -translate-x-1/2 z-200">
+            <h1 class="font-800 text-9 lg-text-20 lg-w180">Our Services</h1>
+
+        </div>
+        <div class="overlay absolute top-0 hfull wfull bg-[#000000] opacity-40"></div>
+    </div>
+</template>
+
+<style></style>
