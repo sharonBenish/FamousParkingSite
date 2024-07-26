200b436dc924072b75e4e72a21ef8f9915a15f90
sharonBenish <sbenishwilliams@gmail.com>
2024-07-26 17:56:49 -0500

    fix: animation bug

--- plugins/aos.js
+++ plugins/aos.js
@@ -4,14 +4,13 @@ import "aos/dist/aos.css";
 export default defineNuxtPlugin((nuxtApp) => {
     if (process.client) {
       nuxtApp.hook('app:mounted', () => {
-        if (window.innerWidth >= 1024){
         AOS.init({
-          disable: 'mobile',
-          duration: 500,
+          disable: false,
+          duration: 600,
           easing: 'ease-in-out-cubic',
           once: true,
           // offset: 200, // Uncomment or adjust as needed
-        })};
+        });
       });
     }
   });
